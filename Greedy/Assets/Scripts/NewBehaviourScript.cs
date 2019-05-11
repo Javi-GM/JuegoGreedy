688be75dd9225c68a6c6ea9009b98cf257aafa68
David Sanchis <dasanmu7@gmail.com>
2019-05-11 22:29:32 +0200

    Revert "Cuenta atras"
    This reverts commit 4f5e6c8de47be25ba62851efbb032785d1923c2e.

--- Greedy/Assets/Scripts/NewBehaviourScript.cs
+++ Greedy/Assets/Scripts/NewBehaviourScript.cs
@@ -6,7 +6,6 @@ public class NewBehaviourScript : MonoBehaviour
 {
     CharacterController characterController;
 
-    [SerializeField] private HealthBarBehaviour healthbar;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
@@ -21,7 +20,6 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        healthbar.SetSize(1f);
     }
 
     // Update is called once per frame
