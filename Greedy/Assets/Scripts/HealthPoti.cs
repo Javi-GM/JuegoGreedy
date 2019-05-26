117de726359d14726db03ca1f5e20fa770e2db35
David Sanchis <dasanmu7@gmail.com>
2019-05-26 21:55:42 +0200

    Añadido el pickup pocion de vida
    Ademas he tenido que volver a añadir todo lo relacionado con los pickups, ya que he estado trabajando en una versión, pero como johans ha hecho merge, me he bajado la versión de johans paravolver a hacer mis cambios ahi y poder hacer push, y ahi no habia nada de eso, se ha tenido que borrar con el merge

---
+++ Greedy/Assets/Scripts/HealthPoti.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoti : MonoBehaviour
+{
+    public int HealthIncrease;
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Rabbit_Red")
+        {
+            other.GetComponent<GreedyMovement>().modifyHealth(HealthIncrease);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Greedy/Assets/Scripts/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public int ExtraLives;
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Rabbit_Red")
+        {
+            other.GetComponent<GreedyMovement>().AddLife(1);
+            Destroy(this.gameObject);
+        }
+    }
+}
